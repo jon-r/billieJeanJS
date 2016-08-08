20505e5684fdbb5a13291fdce3b863ad336b5caf
Jon <jonr.web@gmail.com>
2016-08-09 00:08:29 +0100

    finalised basic interation

--- danceTiles.js
+++ danceTiles.js
@@ -1,49 +1,119 @@
-var DanceMat = function DanceMat(options) {
-  options = options || {};
+var danceMat = {
 
-  this.config = {
-    parentEl : options.hasOwnProperty('parentEl') ? options.parentEl : '#billieJS',
-    height : options.hasOwnProperty('height') ? options.height : 600,
-    width : options.hasOwnProperty('width') ? options.width : 1000,
-    gridSize : options.hasOwnProperty('gridSize') ? options.gridSize : 20,
-  };
+  init : function(options) {
+    var _this = danceMat;
+    options = options || {};
+    _this.config = {
+      parent : options.hasOwnProperty('parentEl') ? options.parentEl : '#billieJS',
+      height : options.hasOwnProperty('height') ? options.height : 600,
+      width : options.hasOwnProperty('width') ? options.width : 1000,
+      gridSize : options.hasOwnProperty('gridSize') ? options.gridSize : 20,
+    };
+    var get = _this.config,
+      el = document.createElement('div');
 
-}
+    _this.sqX = Math.floor(get.width / get.gridSize);
+    _this.sqY = Math.floor(get.height / get.gridSize);
 
-function setCSS(arr) {
+    _this.parentEl = document.querySelector(get.parent);
 
-}
+    _this.parentEl.style.cssText = _this.setParentCss();
+    el.style.cssText = _this.setTileCss();
 
-DanceMat.prototype = {
-  init: function () {
-    this.
+    for (i = 0; i < _this.sqY; i++) {
+      for (j = 0; j < _this.sqX; j++) {
+        var newEl = el.cloneNode(false);
+        newEl.className = 'tile x' + j + ' y' + i;
+        _this.parentEl.appendChild(newEl);
+
+        var newObj = new Tile(newEl,j,i);
+      }
+    }
 
 
   },
+
   setParentCss: function () {
-    var height = this.squaresY * this.gridSize, width = this.squaresX * this.gridSize, cssArr = [
-      'height:' + height + 'px',
-      'width:' + width + 'px',
-      'left: calc(50% - ' + (width / 2) + 'px)',
-      'top: calc(50vh - ' + (height / 2) + 'px)',
-      'position: absolute'
-    ];
+    var get = this.config,
+      height = this.sqY * get.gridSize, width = this.sqX * get.gridSize,
+      cssArr = [
+        'height:' + height + 'px',
+        'width:' + width + 'px',
+        'left: calc(50% - ' + (width / 2) + 'px)',
+        'top: calc(50vh - ' + (height / 2) + 'px)',
+        'position: absolute'
+      ];
     return cssArr.join('; ');
   },
   setTileCss: function () {
-    var height = this.gridSize, width = this.gridSize, cssArr = [
-      'height:' + height + 'px',
-      'width:' + width + 'px',
+    var size = this.config.gridSize, cssArr = [
+      'height:' + size + 'px',
+      'width:' + size + 'px',
       'float: left'
     ];
     return cssArr.join('; ');
-  };
+  }
+
+
+
 
 }
 
-var dancemat = new DanceMat();
+var Tile = function Tile(el, x, y) {
+  el.addEventListener('click', function () { blink.all(x,y)} );
+};
 
-dancemat.init();
+
+var blink = {
+
+  go : function(direction,x,y) {
+    var el = danceMat.parentEl.getElementsByClassName('tile x' + x + ' y' + y);
+    if (el.length == 1) {
+      el[0].classList.add('ping');
+
+      if(direction) {
+        setTimeout(function() {
+          blink[direction](x,y);
+        }, 50);
+      }
+
+      setTimeout(function() {
+        el[0].classList.remove('ping');
+      }, 500);
+    }
+  },
+  up : function(x,y) {
+    y--;
+    blink.go('up',x,y);
+  },
+  down : function(x,y) {
+    y++;
+    blink.go('down',x,y);
+  },
+  left : function(x,y) {
+    x--;
+    blink.go('left',x,y);
+  },
+  right : function(x,y) {
+    x++;
+    blink.go('right',x,y);
+  },
+
+  all : function(x,y) {
+
+    blink.go(false,x,y);
+    setTimeout(function() {
+       blink.up(x,y);
+       blink.down(x,y);
+       blink.left(x,y);
+       blink.right(x,y);
+    }, 50);
+
+  }
+}
+
+
+
 
 
 //function danceMat(options) {
